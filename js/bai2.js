df73c6684174c5f634fdff1549bba7f91483a57b
DatVu42 <vudat42@gmail.com>
2022-05-16 05:47:03 +0700

    bai 4

---
+++ js/bai2.js
@@ -0,0 +1,15 @@
+/**
+ * Khối 1: Input
+ *  số x, số n
+ * 
+ * Khối 2: Các bước xử lý
+ * B1: Tìm các thẻ bằng id
+ * B2: Tạo hàm tính
+ *  + Lấy các giá trị từ form
+ *  + for (var i = 1; i <= n; i++)
+ *      x = x + Math.pow
+ * B3: Gắn hàm vào sự kiện click
+ * 
+ * Khối 3: Output
+ *  tong
+ */
---
+++ js/bai4.js
@@ -0,0 +1,33 @@
+/**
+ * Khối 1: Input
+ * 
+ * Khối 2: Các bước xử lý
+ *  B1: Tạo button và gắn function cho button
+ *  B2: Tạo hàm tao10Div
+ *      for (var i = 1; i <= 10; i++)
+ *          Tạo 1 thẻ Div
+ *          Nếu i là chẵn => set backgroundColor cho thẻ Div = "red"
+ *          Nếu i là lẻ => set backgroundColor cho thẻ Div = "blue"
+ *          Gắn Div vừa tạo lên UI
+ * 
+ * Khối 3: Output
+ *  Tạo 10 thẻ Div
+ *      + Các Div chẵn có background màu đỏ
+ *      + Các Div lẻ có background màu xanh
+ */
+
+function tao10Div() {
+    for (var i = 1; i <= 10; i++) {
+        var taoDiv = document.createElement("div");
+        if (i % 2 == 0) {
+            taoDiv.innerHTML = "Div chẵn " + i;
+            taoDiv.style.backgroundColor = "red";
+        } else {
+            taoDiv.innerHTML = "Div lẻ " + i;
+            taoDiv.style.backgroundColor = "blue";
+        }
+        taoDiv.style.color = "white";
+        taoDiv.style.padding = "15px";
+        document.getElementById("divGroup").appendChild(taoDiv);
+    }
+}   
